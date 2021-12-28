80201ec5a4e7e058b8699ba8a2d713b888e73a16
Joseph Mutua <Mutuaj793@gmail.com>
2021-12-28 21:31:28 +0300

    finish signup

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
-import { axios } from "axios";
+import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify//dist/ReactToastify.min.css";
 
@@ -16,11 +16,38 @@ const Signup = () => {
   const { name, email, password, buttonText } = values;
 
   const handleChange = (value) => (e) => {
-    //
+    setValues({ ...values, [value]: e.target.value });
   };
 
   const handleSubmit = (e) => {
-    //
+    e.preventDefault();
+    setValues({ ...values, buttonText: "Submitting" });
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API}/signup`,
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+      .then((res) => {
+        console.log("SIGNUP SUCCESS", res);
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          buttonText: "Submitted",
+        });
+        toast.success(res.data.message);
+      })
+      .catch((err) => {
+        console.log("SIGNUP ERROR", err.response.data);
+        setValues({ ...values, buttonText: "Submit" });
+        toast.error(err.response.data.error);
+      });
   };
 
   const signupForm = () => (
