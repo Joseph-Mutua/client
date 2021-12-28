f9e4358191885dc8695ebc5c7e40fd3e8d49ecf2
Joseph Mutua <Mutuaj793@gmail.com>
2021-12-28 20:25:07 +0300

    Create signup form

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,19 +1,76 @@
-import React, {useState} from 'react'
-import {Link, Redirect} from "react-router-dom";
-import Layout from '../core/Layout';
-import { axios } from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import "react-toastify//dist/ReactToastify.min.css"
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import Layout from "../core/Layout";
+import { axios } from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify//dist/ReactToastify.min.css";
 
 const Signup = () => {
-    return (
-      <Layout>
+  const [values, setValues] = useState({
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    password: "123456",
+    buttonText: "Submit",
+  });
+
+  const { name, email, password, buttonText } = values;
+
+  const handleChange = (value) => (e) => {
+    //
+  };
+
+  const handleSubmit = (e) => {
+    //
+  };
+
+  const signupForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          value={name}
+          type="text"
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          value={email}
+          type="email"
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          value={password}
+          type="password"
+          className="form-control"
+        />
+      </div>
+
+      <button className="btn btn-primary my-2" onClick={handleSubmit}>
+        {buttonText}
+      </button>
+    </form>
+  );
+
+  return (
+    <Layout>
+      <div className="col-d-6 offset-md-3">
         <ToastContainer />
 
-        <h1>Signup</h1>
-      </Layout>
-    );
-
-}
+        <h1 className="text-center p-5">Signup</h1>
+        {signupForm()}
+      </div>
+    </Layout>
+  );
+};
 
 export default Signup;
